e8aa11e4cd435656cd21bf7310a3c3a720065847
litvinovserg <slitvinov@ukr.net>
2016-04-22 08:47:33 +0300

    Added files via upload

--- arrays_loops_tasks/6.php
+++ arrays_loops_tasks/6.php
@@ -1,12 +1,8 @@
 <?php
+$en = array('green','red','blue');
+$ru = array('зеленый','красный','голубой');
 $arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
-foreach ($arr as $key=>$ru){
-   var_dump($ru);
-}
-
-
-//Дан массив $arr.
-// С помощью цикла foreach запишите английские названия в массив $en,
-// а русские — в массив $ru.
-// $arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
-// $en = array('green', 'red','blue'); $ru = array('зеленый', 'красный', 'голубой');
+foreach($arr as $key=>$value) {
+    $en[] = $key;
+    $ru[] = $value;
+    echo $key;}
